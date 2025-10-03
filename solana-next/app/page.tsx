23733541ecda93fa1b772e9a907d299b31a5458c
Abhishek Kumar <abui8994ty@gmail.com>
2025-10-03 08:00:24 +0530

    sol-next:wallet-connection

---
+++ solana-next/app/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <WalletMultiButton/>
+   </div>     
+  );
+}
